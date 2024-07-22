6a38cf94a7b0f2d43040612f7639c2357215a3e4
spriyha-maurya-iphtech <spriyha.maurya@iphtechnologies.com>
2024-07-22 14:58:38 +0530

    navigation Controller Added

--- UIDesign.xcodeproj/UIDesign/ViewController.swift
+++ UIDesign.xcodeproj/UIDesign/ViewController.swift
@@ -1,3 +1,10 @@
+//
+//  ViewController.swift
+//  UIDesign
+//
+//  Created by iPHTech34 on 03/07/24.
+//
+
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -6,47 +13,47 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var weatherView1: weatherView!
     @IBOutlet weak var energyView1: energyView!
     @IBOutlet weak var devicecollectionView: UICollectionView!
-    @IBOutlet weak var tabBar: CurvedTabBar!
-    @IBOutlet weak var devicesLabel: UILabel!
+    //@IBOutlet weak var tabBar: CurvedTabBar! // Make sure to connect this IBOutlet in the storyboard
+    @IBOutlet weak var tabBar:CurvedTabBar!
     private var addButton: UIButton!
-
+    
     let titles = ["light", "AC", "Climate"]
     let images = ["light", "cooler", "climate"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        // setupCenterButton()
         
         let nib = UINib(nibName: "myCollectionViewCell", bundle: nil)
         devicecollectionView.register(nib, forCellWithReuseIdentifier: "myCollectionViewCell")
     }
     
+    
     func setupUI() {
+        
         let gradientLayer = getGradientLayer(bounds: prohomeLabel.bounds)
         prohomeLabel.textColor = gradientColor(bounds: prohomeLabel.bounds, gradientLayer: gradientLayer)
+        
+        
         setupEnergyView()
         
-        // Add tap gesture recognizer to devicesLabel
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(devicesLabelTapped))
-        devicesLabel.isUserInteractionEnabled = true
-        devicesLabel.addGestureRecognizer(tapGesture)
+        
     }
-
+    
+//    func setupWeatherView() {
+//        weatherView1.setupView(iconImage: UIImage(named: "weathericon"), temperature: "27°", feelsLike: "Feels like 32°", description: "Heavy Rain Tonight")
+//    }
+    
     func setupEnergyView() {
         energyView1.setupView(energy: "16.4 kwh", devices: "3 Device Turn On")
     }
     
-    @objc private func devicesLabelTapped() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        if let secondVC = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController {
-            self.navigationController?.pushViewController(secondVC, animated: true)
-        }
-    }
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return titles.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCollectionViewCell", for: indexPath) as! myCollectionViewCell
         let title = titles[indexPath.item]
